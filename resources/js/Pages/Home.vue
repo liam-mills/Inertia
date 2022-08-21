6a19e34714c93bae9aa74c3449906abffb18195f
Liam <liammills.web@gmail.com>
2022-08-21 21:31:35 +1000

    Passing data from server side route to Vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hello {{ name }}.</h1>
+
+    <p>We'll be using the following frameworks:</p>
+    <ul>
+        <li v-for="framework in frameworks">{{ framework }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array
+    }
+};
+</script>
