86a309785fd45ead7d98491c7473e747fded1326
Liam <liammills.web@gmail.com>
2022-08-22 20:48:23 +1000

    Add tailwind

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,9 +1,10 @@
 <template>
-    <nav>
-        <ul>
+    <nav class="mt-6">
+        <ul class="list-disc">
             <li><Link href="/">Home</Link></li>
             <li><Link href="/users">Users</Link></li>
             <li><Link href="/settings">Settings</Link></li>
+            <li><Link href="/logout" method="post" as="button">Log Out</Link></li>
         </ul>
     </nav>
 </template>
