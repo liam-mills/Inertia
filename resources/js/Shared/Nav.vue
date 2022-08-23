34de04928d51f6aea5feb07859db8efe92a2c0f1
Liam <liammills.web@gmail.com>
2022-08-23 19:28:46 +1000

    NavLink component

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -2,31 +2,13 @@
     <nav class="mt-6">
         <ul class="list-disc">
             <li>
-                <Link
-                    href="/"
-                    class="text-blue-500 hover:underline"
-                    :class="{'font-bold underline': $page.component === 'Home'}"
-                >
-                Home
-                </Link>
+                <NavLink href="/" :active="$page.component === 'Home'">Home</NavLink>
             </li>
             <li>
-                <Link
-                    href="/users"
-                    class="text-blue-500 hover:underline"
-                    :class="{'font-bold underline': $page.component === 'Users'}"
-                >
-                Users
-                </Link>
+                <NavLink href="/users" :active="$page.component === 'Users'">Users</NavLink>
             </li>
             <li>
-                <Link
-                    href="/settings"
-                    class="text-blue-500 hover:underline"
-                    :class="{'font-bold underline': $page.component === 'Settings'}"
-                >
-                Settings
-                </Link>
+                <NavLink href="/settings" :active="$page.component === 'Settings'">Settings</NavLink>
             </li>
             <li><Link href="/logout" method="post" as="button" class="text-blue-500 hover:underline">Log Out</Link></li>
         </ul>
@@ -35,8 +17,9 @@
 
 <script>
 import { Link } from '@inertiajs/inertia-vue3';
+import NavLink from '../Shared/NavLink';
 
 export default {
-    components: { Link }
+    components: { Link, NavLink }
 };
 </script>
